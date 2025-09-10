676d1b0476b50d0e8032dad81f66cfde5bc00548
Praveen N <praveen.n7050@gmail.com>
2025-09-10 06:30:45 +0530

    Update Array and objects.js

--- js practice/Array and objects.js
+++ js practice/Array and objects.js
@@ -41,4 +41,55 @@ let digit=[10,20,40,50,70,80]
 
 let find=digit.find(digits=>digits<90);
 console.log(find)
-----------------------
+
+
+// includes
+
+const array=[70,"hello",60,"BYE",'Some'];
+
+console.log(array.includes("hello"))
+
+
+// last  indexof()
+
+num=[20,30,40,50,20,30,30]
+
+console.log(num.lastIndexOf(30))
+
+
+
+// at()
+
+
+numer=[20,30,40,50,20,30,30]
+
+console.log(numer.at(4))
+
+// join()
+
+let bike=["wheel","motor","horn","brake","gear"]
+
+console.log(bike.join())
+
+
+
+// fill()
+
+let bikes=["wheel","motor","horn","brake","gear"]
+
+console.log(bikes.fill("acclerator",2,3))
+
+
+
+// flat()
+
+flat=[1,2,[3],4,[5],[6],[7,[8,9,10]],[11]]
+
+console.log(flat.flat(2))
+
+const name=["praveen","surya","tharun","mani","prem"]
+
+name.forEach(friend => {
+    console.log(friend)
+});
+
